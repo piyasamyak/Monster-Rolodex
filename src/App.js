0dab9f848f59808d73c0c7d53d6da330b3d403a2
Samyak Piya <samyakpiya@gmail.com>
2023-01-28 11:42:10 -0500

    Learned Array.map()

--- src/App.js
+++ src/App.js
@@ -7,34 +7,30 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'Samyak',
-      company: 'UKITS'
-    }
+      monsters: [
+        {
+          name: 'Linda',
+          id: "123asd"
+        },
+        {
+          name: 'Frank',
+          id: "123szxc"
+        },
+        {
+          name: 'Jacky',
+          id: "a213asc"
+        }]
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name}, I work at {this.state.company}!
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: 'Aastha', company: 'CARE',
-                  };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >Change Name</button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <div key={monster.id}><h1>{monster.name}</h1></div>;
+          })
+        }
       </div>
     )
   }
