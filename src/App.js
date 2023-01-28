ca303b2ac54f3f90c0b31a7433e0dcbce4393a36
Samyak Piya <samyakpiya@gmail.com>
2023-01-28 11:17:42 -0500

    Learned and implemented setState()

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Samyak',
+      company: 'UKITS'
+    }
+  }
 
   render() {
     return (
@@ -11,16 +18,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to refresh.
+            Hi {this.state.name}, I work at {this.state.company}!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => {
+            this.setState({ name: 'Aastha', company: 'CARE' });
+          }}>Change Name</button>
         </header>
       </div>
     )
